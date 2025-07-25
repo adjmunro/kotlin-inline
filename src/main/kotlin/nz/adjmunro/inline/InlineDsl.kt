aa77882592f62f4adf0874c55c9502bf0209c288
Adam Munro <adam.munro@orbitremit.com>
2025-07-25 23:46:06 +1200

    feat: add InlineDsl

--- src/main/kotlin/nz/adjmunro/inline/InlineDsl.kt
+++ src/main/kotlin/nz/adjmunro/inline/InlineDsl.kt
@@ -1,1 +1,8 @@
-package nz.adjmunro.inline 
+package nz.adjmunro.inline
+
+/**
+ * Annotation to mark functions and types as members of the Inline DSL library.
+ */
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.TYPEALIAS, AnnotationTarget.PROPERTY)
+@DslMarker
+public annotation class InlineDsl
